9dc235e7575972e7502a75a7f55a074c6d9b348d
fuonghung210a <phamduynam986@gmail.com>
2024-01-26 07:35:48 +0700

    prac1

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,15 +1,28 @@
-import 'dart:ffi';
+/// hãy viết code bằng tất cả các cách có thể để in ra:
+/// `Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (ph ần 1)`
+/// gợi ý: sử dụng hàm subString()
+void main(List<String> arguments) {
+  String string =
+      ' đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...';
 
-void main(List<String> args) {
-  String str =
-      " đây là kết quả của buổi học thứ 2 về dart: dart basics (phần 1)...";
+  String result = string
+      .trim()
+      .vietHoaChuCaiDau()
+      .replaceFirst('của ', '')
+      .replaceFirst('dart', 'DART')
+      .replaceFirst('dart basics', 'DART BASICS')
+      .replaceAll('...', '');
+  print(result);
+}
 
-  /// hãy viết code bằng tất cả các cách có thể để in ra:
-  /// `Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1)`
-  List<String> arr = str.trim().split(" ");
-  arr[0].replaceAll("đ", "Đ");
-
-  for (String str in arr) {
-    print(str);
-  }
+extension StringExtension on String {
+  String vietHoaChuCaiDau() {
+    if (isEmpty) {
+      return this;
+    } else {
+      return this[0].toUpperCase() +
+          substring(1)
+              .toLowerCase(); //viết hoa phần tử đầu, còn lại viết thường hết
+    }
+  } /*  */
 }
