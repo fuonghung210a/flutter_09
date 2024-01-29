18ba9cf06a2684de54af5b664b6979cb7871bf32
fuonghung210a <phamduynam986@gmail.com>
2024-01-29 20:20:27 +0700

    hihi

--- prac1.dart
+++ prac1.dart
@@ -1,12 +1,50 @@
+import 'dart:ffi';
+
 void main() {
-  String s = '''Hello world!''';
+  void workingWithString() {
+    String s = '''Hello world!''';
 
-  String s1 = 'Đây là đoạn văn được viết bằng tiếng việt';
+    String s1 = 'Đây là đoạn văn được viết bằng tiếng việt';
 
-  String s2 = 'This is a sentence written in English';
-  List<String> str = s2.split(" ");
+    String s2 = 'This is a sentence written in English';
+    List<String> str = s2.split(" ");
 
-  print(str.contains("This"));
-  print("s2.toUpperCase() : ${s2.toLowerCase()}");
-  print("Index of \'Sentence'\ is: ${str.lastIndexOf("sentence")}");
+    print(s2.replaceAll(
+        'i', 'Replace')); //rrturn a string without changing the source
+    print(s);
+  }
+
+  ;
+  testArray();
 }
+
+void testArray() {
+  List strs = [
+    "a",
+    "b",
+    "c",
+    10,
+    20,
+    -1,
+    1.0,
+    true,
+    false,
+  ];
+  List<String> strs_1 = <String>["a", "adas", "d"];
+
+  List<String> subList = strs_1.sublist(0);
+  print(subList);
+}
+
+void workingWithDouble() {
+  double? a = double.tryParse("99.a");
+  print(a);
+}
+
+void workingWithInteger() {
+  int a = -21;
+  int b = 221;
+
+  print(a + b);
+}
+// 
